1d82ea5941b25cf464093b169d02b37eb72092fe
Eoghan Maxwell <eoghan.maxwell@cegedim-activ.com>
2018-03-30 17:10:54 +0200

    Removing unnecessary files

--- src/elm/Components/Hello.elm
+++ src/elm/Components/Hello.elm
@@ -2,11 +2,19 @@ module Components.Hello exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import String
 
 -- hello component
 hello : Int -> Html a
 hello model =
   div
-    [ class "h1" ]
-    [ text ( "Hello, Elm" ++ ( "!" |> String.repeat model ) ) ]
+    []
+    [
+        div [ class "h1" ]
+        [
+            text ( "Hello Elm" )
+        ],
+        div []
+        [
+            text ( "Count: " ++ ( toString model ) )
+        ]
+    ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,7 +3,6 @@ import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing ( onClick )
 
--- component import example
 import Components.Hello exposing ( hello )
 
 
@@ -21,43 +20,35 @@ model = 0
 
 
 -- UPDATE
-type Msg = NoOp | Increment
+type Msg = NoOp | Increment | Decrement
 
 update : Msg -> Model -> Model
 update msg model =
   case msg of
     NoOp -> model
     Increment -> model + 1
+    Decrement -> model - 1
 
 
 -- VIEW
--- Html is defined as: elem [ attribs ][ children ]
--- CSS can be applied via class names or inline style attrib
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][    -- inline CSS (literal)
+  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][
     div [ class "row" ][
       div [ class "col-xs-12" ][
         div [ class "jumbotron" ][
-          img [ src "static/img/elm.jpg", style styles.img ] []                             -- inline CSS (via var)
-          , hello model                                                                     -- ext 'hello' component (takes 'model' as arg)
-          , p [] [ text ( "Elm Webpack Starter" ) ]
-          , button [ class "btn btn-primary btn-lg", onClick Increment ] [                  -- click handler
-            span[ class "glyphicon glyphicon-star" ][]                                      -- glyphicon
-            , span[][ text "FTW!" ]
-          ]
+          hello model
+          , createButton Increment "Increment"
+          , createButton Decrement "Decrement"
         ]
       ]
     ]
   ]
 
-
--- CSS STYLES
-styles : { img : List ( String, String ) }
-styles =
-  {
-    img =
-      [ ( "width", "33%" )
-      , ( "border", "4px solid #337AB7")
-      ]
-  }
+createButton: Msg -> String -> Html Msg
+createButton clickMessage label =
+    button [ class "btn btn-primary btn-lg", onClick clickMessage ]
+    [
+      span[ class "glyphicon glyphicon-star" ][]
+      , span[][ text label ]
+    ]
