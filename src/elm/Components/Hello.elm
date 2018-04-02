eb3e04d0ade26667741c6a8ed9c4481a74bde3b1
emaxwell14 <emaxwell1414@hotmail.com>
2018-04-02 16:27:59 +0200

    Updating with formatter

--- src/elm/Components/Hello.elm
+++ src/elm/Components/Hello.elm
@@ -3,18 +3,20 @@ module Components.Hello exposing (..)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 
+
 -- hello component
+
+
 hello : Int -> Html a
 hello model =
-  div
-    []
-    [
-        div [ class "h1" ]
-        [
-            text ( "Hello Elm" )
-        ],
-        div []
-        [
-            text ( "Count: " ++ ( toString model ) )
+    div
+        []
+        [ div
+            [ class "h1"
+            ]
+            [ text ("Hello Elm")
+            ]
+        , div []
+            [ text ("Count: " ++ (toString model))
+            ]
         ]
-    ]
--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,54 +1,77 @@
 module Main exposing (..)
+
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing ( onClick )
-
-import Components.Hello exposing ( hello )
+import Html.Events exposing (onClick)
+import Components.Hello exposing (hello)
 
 
 -- APP
+
+
 main : Program Never Model Msg
 main =
-  Html.beginnerProgram { model = model, view = view, update = update }
+    Html.beginnerProgram { model = model, view = view, update = update }
+
 
 
 -- MODEL
-type alias Model = Int
+
+
+type alias Model =
+    Int
+
 
 model : Model
-model = 0
+model =
+    0
+
 
 
 -- UPDATE
-type Msg = NoOp | Increment | Decrement
+
+
+type Msg
+    = NoOp
+    | Increment
+    | Decrement
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    NoOp -> model
-    Increment -> model + 1
-    Decrement -> model - 1
+    case msg of
+        NoOp ->
+            model
+
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
 
 
 -- VIEW
+
+
 view : Model -> Html Msg
 view model =
-  div [ class "container", style [("margin-top", "30px"), ( "text-align", "center" )] ][
-    div [ class "row" ][
-      div [ class "col-xs-12" ][
-        div [ class "jumbotron" ][
-          hello model
-          , createButton Increment "Increment"
-          , createButton Decrement "Decrement"
+    div [ class "container", style [ ( "margin-top", "30px" ), ( "text-align", "center" ) ] ]
+        [ div [ class "row" ]
+            [ div [ class "col-xs-12" ]
+                [ div [ class "jumbotron" ]
+                    [ hello model
+                    , createButton Increment "Increment"
+                    , createButton Decrement "Decrement"
+                    ]
+                ]
+            ]
         ]
-      ]
-    ]
-  ]
 
-createButton: Msg -> String -> Html Msg
+
+createButton : Msg -> String -> Html Msg
 createButton clickMessage label =
     button [ class "btn btn-primary btn-lg", onClick clickMessage ]
-    [
-      span[ class "glyphicon glyphicon-star" ][]
-      , span[][ text label ]
-    ]
+        [ span [ class "glyphicon glyphicon-star" ] []
+        , span [] [ text label ]
+        ]
